1485d0425e3b3ba9c3f0d8419545c9ec4373bc08
keziapranata <132723683+keziapranata@users.noreply.github.com>
2026-02-03 14:24:45 +1100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,13 +6,13 @@ export const metadata: Metadata = {
   description:
     "Hi baby, come play a memory game :) I hope this makes you smile.. Please open lewat laptop ya, and set aside 15 minutes!!",
   openGraph: {
-    title: "Just for Daryl :)",
-    description:
-      "Hi baby, come play a memory game :) I hope this makes you smile.. Please open lewat laptop ya, and set aside 15 minutes!!",
-    url: "https://i-love-you-daryl.vercel.app/",
-    type: "website",
-    siteName: "Just for Daryl :)",
-  },
+  title: "Just for Daryl :)",
+  description:
+    "Hi baby, come play a memory game :) I hope this makes you smile.. Please open lewat laptop ya, and set aside 15 minutes!!",
+  url: "https://i-love-you-daryl.vercel.app/",
+  type: "website",
+  images: [{ url: "/cute-cats.jpg", width: 1200, height: 630, alt: "Just for Daryl 💖" }],
+},
 };
 
 export default function RootLayout({
