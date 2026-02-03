7a31c8e05ba9fbe674577ca6d1d49af3b27652ba
keziapranata <132723683+keziapranata@users.noreply.github.com>
2026-02-03 14:38:43 +1100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,25 @@
 import type { Metadata } from "next";
-import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://i-love-you-daryl.vercel.app"),
   title: "Just for Daryl :)",
   description:
-    "Hi baby, come play a memory game with me :) Open lewat laptop ya, I hope this makes you smile.. Set aside 15 mins!",
-  keywords: [
-    "Valentine's card game",
-    "romantic proposal game",
-    "photo card challenge",
-    "Valentine's Day surprise",
-    "couples game",
-    "valentine's day game",
-    "proposal game",
-  ],
+    "Hi baby, come play a memory game :) I hope this makes you smile.. Please open lewat laptop ya, and set aside 15 minutes!!",
+  openGraph: {
+    title: "Just for Daryl :)",
+    description:
+      "Hi baby, come play a memory game :) I hope this makes you smile.. Please open lewat laptop ya, and set aside 15 minutes!!",
+    url: "https://i-love-you-daryl.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "/og.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Just for Daryl 💖",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
