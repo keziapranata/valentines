a48cf174ef2552a0ca55f50638f30406e18f1f65
keziapranata <132723683+keziapranata@users.noreply.github.com>
2026-02-03 13:11:01 +1100

    Update TextFooter.tsx

--- src/components/TextFooter.tsx
+++ src/components/TextFooter.tsx
@@ -10,18 +10,18 @@ export default function TextFooter() {
   return (
     <>
       {/* Left Text */}
-      <h1
-        className={`absolute left-10 bottom-5 transform -translate-y-1/2 text-white text-5xl font-bold leading-tight ${playfairDisplay.className}`}
-      >
-        <span className="text-gray-400">Match</span> <br /> the photo pairs
-      </h1>
+<h1
+  className={`absolute left-10 top-10 text-white text-5xl font-bold leading-tight ${playfairDisplay.className}`}
+>
+  <span className="text-gray-400">Match</span> <br /> the photo pairs
+</h1>
 
       {/* Right Text */}
-      <h1
-        className={`absolute right-10 bottom-5 transform -translate-y-1/2 text-white text-5xl font-bold leading-tight text-right ${playfairDisplay.className}`}
-      >
-        to reveal <br /> <span className="text-gray-400">the surprise...</span>
-      </h1>
+<h1
+  className={`absolute right-10 top-10 text-white text-5xl font-bold leading-tight text-right ${playfairDisplay.className}`}
+>
+  to reveal <br /> <span className="text-gray-400">the surprise...</span>
+</h1>
 
       {/* Copyright */}
       <p className="absolute bottom-5 right-5 text-white text-sm opacity-15 hover:opacity-50">
